8a997ec30aad2967486f614ac642b6abcc00c3b2
Mike Littrell <mike.littrell@returnpath.com>
2015-12-09 10:57:58 -0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s $HOME/Dot-Files/.vim $HOME/.vim
+ln -s $HOME/Dot-Files/.vimrc $HOME/.vimrc
+ln -s $HOME/Dot-Files/.zshrc $HOME/.zshrc
+ln -s $HOME/Dot-Files/.gitignore $HOME/.gitignore
+ln -s $HOME/Dot-Files/.gitconfig $HOME/.gitconfig
+
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
